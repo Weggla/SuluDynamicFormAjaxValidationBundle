1c925ce5850b02ddafe6b77df75dbe245133d7ae
Weggla <weggla@posteo.de>
2024-09-17 23:40:56 +0200

    Add bundle services

--- src/SuluDynamicFormAjaxValidationBundle.php
+++ src/SuluDynamicFormAjaxValidationBundle.php
@@ -4,9 +4,20 @@ declare(strict_types=1);
 
 namespace Sulu\Bundle\DynamicFormAjaxValidation;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class SuluDynamicFormAjaxValidationBundle extends Bundle
+class SuluDynamicFormAjaxValidationBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        parent::loadExtension($config, $container, $builder);
+
+        $loader = new YamlFileLoader($builder, new FileLocator(__DIR__.'/config'));
+        $loader->load('services.yaml');
+    }
 
 }
